6c0386e10ad0f8e4f186cf4024f8a438ac708045
Zaka Ullah <zakaullahzf@gmail.com>
2023-08-15 18:18:00 +0500

    Format Document Prettier

--- App.tsx
+++ App.tsx
@@ -1,34 +1,34 @@
-import {useState } from 'react';
-import { StyleSheet, View,FlatList} from 'react-native';
-import TodoItem from './components/TodoItem';
-import TodoInput from './components/TodoInput';
+import { useState } from "react";
+import { StyleSheet, View, FlatList } from "react-native";
+import TodoItem from "./components/TodoItem";
+import TodoInput from "./components/TodoInput";
 
 export default function App() {
+  const [todosList, setTodosList] = useState([]);
 
-   const [todosList, setTodosList]= useState([]);
-
-  
-  function addTodoText(enteredTodoText){
-    if(enteredTodoText != ""){
-      setTodosList([...todosList, {text: enteredTodoText, id: Math.random().toString()}]);
+  function addTodoText(enteredTodoText) {
+    if (enteredTodoText != "") {
+      setTodosList([
+        ...todosList,
+        { text: enteredTodoText, id: Math.random().toString() },
+      ]);
     }
-    
-
   }
 
   return (
     <View style={styles.container}>
-     <TodoInput onTodoAdded= {addTodoText}/>
-      
-      <FlatList style={styles.todoList} data= {todosList} renderItem={(todoItem) => {
-        return(
-          <TodoItem text={todoItem.item.text}/>
-        );
-      }} keyExtractor={(item,index)=> {
-        return (
-          item.id
-        );
-      }} />
+      <TodoInput onTodoAdded={addTodoText} />
+
+      <FlatList
+        style={styles.todoList}
+        data={todosList}
+        renderItem={(todoItem) => {
+          return <TodoItem text={todoItem.item.text} />;
+        }}
+        keyExtractor={(item, index) => {
+          return item.id;
+        }}
+      />
     </View>
   );
 }
@@ -38,18 +38,15 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 20,
   },
-  
-  
+
   todoList: {
-    borderColor: '#cccccc',
+    borderColor: "#cccccc",
     borderWidth: 1,
     fontSize: 24,
     marginVertical: 10,
-    textAlign: 'center',
+    textAlign: "center",
     borderRadius: 10,
     paddingTop: 10,
-    paddingBottom: 10,   
+    paddingBottom: 10,
   },
-  
-  
 });
